c3a4dda60ae09de8087abb601a2927bc9e426e90
pooulad <poooooladi@gmail.com>
2023-07-05 14:06:49 +0330

    setup about section

---
+++ src/components/About/components/index.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from "react";
+
+function About() {
+  return (
+    <div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+    </div>
+  );
+}
+
+export default memo(About);
