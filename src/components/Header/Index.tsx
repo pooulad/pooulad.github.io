2b09d034c9c7044638eb48556c28eb9fae35d597
pooulad <poooooladi@gmail.com>
2023-07-05 15:59:44 +0330

    create header component

---
+++ src/components/Header/Index.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>Header</div>
+      <div>Header</div>
+      <div>Header</div>
+      <div>Header</div>
+    </div>
+  );
+}
+
+export default memo(Header);
