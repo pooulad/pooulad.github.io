32aaa9def8baa51eb12303e864040c05c1bd2acc
pooulad <poooooladi@gmail.com>
2023-07-05 14:12:00 +0330

    setup main components

---
+++ src/components/Main/components/index.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from "react";
+
+function Main() {
+  return (
+    <div>
+      <div>Main</div>
+      <div>Main</div>
+      <div>Main</div>
+      <div>Main</div>
+      <div>Main</div>
+    </div>
+  );
+}
+
+export default memo(Main);
