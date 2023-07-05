bcad91ae6e066ecf01ddc03eca50f1299e398034
pooulad <poooooladi@gmail.com>
2023-07-05 13:23:44 +0330

    Create main.tsx

---
+++ src/pages/test/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function main() {
+  return (
+    <div>
+      <div>main</div>
+      <div>main</div>
+      <div>main</div>
+      <div>main</div>
+      <div>main</div>
+      <div>main</div>
+    </div>
+  );
+}
+
+export default main;
