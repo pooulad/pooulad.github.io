130c8484d44bea1fbe8c1927cabe3fecc8a6eecc
pooulad <poooooladi@gmail.com>
2023-07-05 14:12:18 +0330

    setup contact components

---
+++ src/components/Contact/components/index.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from "react";
+
+function Contact() {
+  return (
+    <div>
+      <div>Contact</div>
+      <div>Contact</div>
+      <div>Contact</div>
+      <div>Contact</div>
+      <div>Contact</div>
+      <div>Contact</div>
+    </div>
+  );
+}
+
+export default memo(Contact);
---
+++ src/components/Contact/types/index.ts
@@ -0,0 +1,3 @@
+export interface ContactProps {
+  name: string;
+}
