bc43bb406efc2b49ec3a87cf4fd4bd7ce1b431b2
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 10:16:29 +0330

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    // <ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-light w-full min-h-screen">
+        <Component {...pageProps} />
+      </main>
+    </>
+    // </ThemeProvider>
   );
 }
