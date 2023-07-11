9c9452338e6758177d78da2a2e11803eaedc4a98
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 10:17:42 +0330

    add links and logo in header component

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,5 +1,7 @@
 import { useTheme } from "next-themes";
+import Link from "next/link";
 import React, { memo, useEffect, useState } from "react";
+import Logo from "./Logo";
 
 function Header() {
   const { theme, setTheme } = useTheme();
@@ -7,13 +9,36 @@ function Header() {
   useEffect(() => setHasMounted(true), []);
   if (!hasMounted) return null;
   return (
-    <div>
-      <div className="text-3xl font-bold underline">
-        The current theme is: {theme}
+    <header className="w-full px-32 py-8 font-medium flex justify-between items-center">
+      <nav className="text-3xl font-bold underline">
+        {/* The current theme is: {theme}
         <button onClick={() => setTheme("light")}>روشن</button>
-        <button onClick={() => setTheme("dark")}>تیره</button>
+        <button onClick={() => setTheme("dark")}>تیره</button> */}
+        <Link href={"/"}>خانه</Link>
+        <Link href={"/about"}>درباره من</Link>
+        <Link href={"/projects"}>پروژه ها</Link>
+      </nav>
+      <nav>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
       </div>
-    </div>
+    </header>
   );
 }
 
