a317e36876e5f04174a63b8ca21dba6cb1a6e571
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 14:09:00 +0330

    Update index.ts

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -1,2 +1,4 @@
 export {default as GithubIcon} from "./GithubIcon"
 export {default as InstagramIcon} from "./InstagramIcon"
+export {default as AmirankalaIcon} from "./AmirankalaIcon"
+export {default as TelegramIcon} from "./TelegramIcon"
