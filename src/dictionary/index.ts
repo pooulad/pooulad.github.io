a079ce05f90ecc5004a5dfe929ff9e5ffed7a6c0
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 15:25:19 +0330

    add new titles in dictionary

--- src/dictionary/index.ts
+++ src/dictionary/index.ts
@@ -1,9 +1,13 @@
 export const dictionary = {
-    header : {
-        links : {
-            home : "خانه",
-            projects : "پروژه ها",
-            about : "درباره من"
-        }
-    }
-}
+  header: {
+    links: {
+      home: "خانه",
+      projects: "پروژه ها",
+      about: "درباره من",
+    },
+  },
+  owner: "امیرمهدی پولادی",
+  ownerEn: "amir mahdi pouladi",
+  headTitle: "سایت شخصی امیرمهدی پولادی",
+  metaTitle: "وبسایت رزومه امیرمهدی پولادی",
+};
