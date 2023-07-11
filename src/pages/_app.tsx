78f12ebafe0fc72d3cfef5393929b0b759f0e935
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 15:25:27 +0330

    add header in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
+import Header from "@/components/Header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-light w-full min-h-screen">
+        <Header />
         <Component {...pageProps} />
       </main>
     </>
