eb5009e04a2de6534875d67d31bfbe50e2797dda
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 10:37:53 +0330

    create new custom file for return link

---
+++ src/components/Common/CustomLink/Index.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React, { memo } from "react";
+
+export type CustomLinkProps = {
+  title: string;
+  classname: string;
+  link: string;
+};
+
+function CustomLink({ title, classname, link }: CustomLinkProps) {
+  return (
+    <Link href={link} className={classname}>
+      {title}
+    </Link>
+  );
+}
+
+export default memo(CustomLink);
