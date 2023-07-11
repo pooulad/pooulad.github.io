211ad95e3fecd76edd2662c9a40d1409a989727f
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 12:20:58 +0330

    handle active link in header

--- src/components/Common/CustomLink/CustomLink.tsx
+++ src/components/Common/CustomLink/CustomLink.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { memo } from "react";
 
 export type CustomLinkProps = {
@@ -8,10 +9,15 @@ export type CustomLinkProps = {
 };
 
 function CustomLink({ title, classname, link }: CustomLinkProps) {
+  const router = useRouter();
   return (
     <Link href={link} className={`${classname} relative group`}>
       {title}
-      <span className="h-[1px] inline-block w-full bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300">
+      <span
+        className={`h-[1px] inline-block bg-dark absolute right-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === link ? "w-full" : "w-0"
+        }`}
+      >
         &nbsp;
       </span>
     </Link>
