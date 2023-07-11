cc3d9a6208dd4ba3059bdbee6a5e0991a79b2df3
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 11:21:47 +0330

    Create Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,47 @@
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import React, { memo, useEffect, useState } from "react";
+import Logo from "./Logo";
+import { CustomLink } from "../Common";
+import { dictionary } from "@/dictionary";
+
+function Header() {
+  const { theme, setTheme } = useTheme();
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => setHasMounted(true), []);
+  if (!hasMounted) return null;
+  return (
+    <header className="w-full px-32 py-8 font-medium flex justify-between items-center">
+      <nav>
+        {/* The current theme is: {theme}
+        <button onClick={() => setTheme("light")}>روشن</button>
+        <button onClick={() => setTheme("dark")}>تیره</button> */}
+        <CustomLink title={dictionary.header.links.home} link={"/"} classname="mr-4" />
+        <CustomLink title={dictionary.header.links.projects} link={"/about"} classname="mx-4" />
+        <CustomLink title={dictionary.header.links.skills} link={"/projects"} classname="ml-4" />
+      </nav>
+      <nav>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+        <Link href={"/"} target={"_blank"}>
+          T
+        </Link>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+}
+
+export default memo(Header);
