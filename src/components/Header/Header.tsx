01eefd4a64172bf489487832feb4856718619e86
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-11 13:09:08 +0330

    add all basic links for icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,26 +17,38 @@ function Header() {
         {/* The current theme is: {theme}
         <button onClick={() => setTheme("light")}>روشن</button>
         <button onClick={() => setTheme("dark")}>تیره</button> */}
-        <CustomLink title={dictionary.header.links.home} link={"/"} classname="mr-4" />
-        <CustomLink title={dictionary.header.links.about} link={"/about"} classname="mx-4" />
-        <CustomLink title={dictionary.header.links.projects} link={"/projects"} classname="ml-4" />
+        <CustomLink
+          title={dictionary.header.links.home}
+          link={"/"}
+          classname="mr-4"
+        />
+        <CustomLink
+          title={dictionary.header.links.about}
+          link={"/about"}
+          classname="mx-4"
+        />
+        <CustomLink
+          title={dictionary.header.links.projects}
+          link={"/projects"}
+          classname="ml-4"
+        />
       </nav>
-      <nav>
-        <Link href={"/"} target={"_blank"}>
+      <nav className="flex items-center justify-center flex-wrap">
+        <a href={"https://amirankala.com"} target={"_blank"}>
           <GithubIcon />
-        </Link>
-        <Link href={"/"} target={"_blank"}>
-          T
-        </Link>
-        <Link href={"/"} target={"_blank"}>
-          T
-        </Link>
-        <Link href={"/"} target={"_blank"}>
-          T
-        </Link>
-        <Link href={"/"} target={"_blank"}>
-          T
-        </Link>
+        </a>
+        <a href={"https://github.com/pooulad"} target={"_blank"}>
+          <GithubIcon />
+        </a>
+        <a href={"https://t.me/pouladpesar"} target={"_blank"}>
+          <GithubIcon />
+        </a>
+        <a href={"https://instagram.com/_poulad_"} target={"_blank"}>
+          <GithubIcon />
+        </a>
+        <a href={"/"} target={"_blank"}>
+          <GithubIcon />
+        </a>
       </nav>
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
