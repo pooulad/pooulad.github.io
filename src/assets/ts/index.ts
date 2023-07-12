4b6672d67321907701919f5c212b8aa8fd189aaf
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 14:06:25 +0330

    update index.ts

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -1,6 +1,7 @@
-export {default as GithubIcon} from "./GithubIcon"
-export {default as InstagramIcon} from "./InstagramIcon"
-export {default as AmirankalaIcon} from "./AmirankalaIcon"
-export {default as TelegramIcon} from "./TelegramIcon"
-export {default as EmailIcon} from "./EmailIcon"
-export {default as LinkIcon} from "./LinkIcon"
+export { default as GithubIcon } from "./GithubIcon";
+export { default as InstagramIcon } from "./InstagramIcon";
+export { default as AmirankalaIcon } from "./AmirankalaIcon";
+export { default as TelegramIcon } from "./TelegramIcon";
+export { default as EmailIcon } from "./EmailIcon";
+export { default as LinkIcon } from "./LinkIcon";
+export { default as HiremeIcon } from "./HiremeIcon";
