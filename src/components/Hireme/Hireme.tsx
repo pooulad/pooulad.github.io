3d3572f0402f2b30e10a9e4e8e92d3019097e549
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 14:14:20 +0330

    add ui for hire me icon

--- src/components/Hireme/Hireme.tsx
+++ src/components/Hireme/Hireme.tsx
@@ -1,10 +1,12 @@
+import { HiremeIcon } from "@/assets/ts";
 import React, { memo } from "react";
 
 function Hireme() {
   return (
-    <div>
-      <div>Hireme</div>
-      <div>Hireme</div>
+    <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
+      <div className="w-48 flex items-center justify-center relative h-auto">
+        <HiremeIcon className="fill-dark animate-spin-slow" />
+      </div>
     </div>
   );
 }
