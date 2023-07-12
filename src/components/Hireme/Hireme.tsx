c16f3a42aa2bcb577c00ace6b67f24ee895769bf
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 14:07:37 +0330

    Create Hireme.tsx

---
+++ src/components/Hireme/Hireme.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+
+function Hireme() {
+  return (
+    <div>
+      <div>Hireme</div>
+      <div>Hireme</div>
+    </div>
+  );
+}
+
+export default memo(Hireme);
