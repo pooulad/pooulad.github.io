9eb4ace74d8720c71c9bb70a6f2941c1cab1b39b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 11:53:48 +0330

    add animated text in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import Layout from "@/Layout";
 import Image from "next/image";
 import { dictionary } from "@/dictionary";
 import ProfilePic from "../assets/images/profile-pic.png";
+import AnimatedText from "@/components/Common/AnimatedText/AnimatedText";
 
 export default function Home() {
   return (
@@ -19,10 +20,14 @@ export default function Home() {
         <meta name="description" content={dictionary.metaTitle} />
       </Head>
       <main className="flex items-center text-dark w-full min-h-screen">
-        <Layout>
+        <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
-            <div>
+            <div className="w-1/2">
               <Image src={ProfilePic} alt={dictionary.owner} className="w-full h-auto" />
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText text={dictionary.home.h1Tag} className="!text-6xl !text-right" />
+              <p>{dictionary.home.pTag}</p>
             </div>
           </div>
         </Layout>
