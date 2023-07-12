c3b7474a0bc03d1622ea315a6a4a7b7cc28da440
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 15:29:04 +0330

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,12 @@ import { ThemeProvider } from "next-themes";
 import Head from "next/head";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import { Fragment } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // <ThemeProvider>
-    <>
+    <Fragment>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -18,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <Footer />
       </main>
-    </>
+    </Fragment>
     // </ThemeProvider>
   );
 }
