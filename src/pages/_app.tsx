a2eb11449456e0bcf8410bc224cdf3de8cc6e276
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 14:07:08 +0330

    add footer in project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className="bg-light w-full min-h-screen">
         <Header />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
     // </ThemeProvider>
