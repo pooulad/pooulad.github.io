173bf3ec485a24880be04e3741b9bf03098b81f4
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 14:34:20 +0330

    change btn ui

--- src/components/Hireme/Hireme.tsx
+++ src/components/Hireme/Hireme.tsx
@@ -9,7 +9,7 @@ function Hireme() {
       <div className="w-48 flex items-center justify-center relative h-auto">
         <HiremeIcon className="fill-dark animate-spin-slow" />
         <Link
-          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark"
           href={`mailto:${dictionary.ownerEmailAddress}`}
           target={"_blank"}
         >
