8b6cdac07a6cbedfdadef5a4e9408a8bf23fbdab
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 14:14:06 +0330

    customize animate spin in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
