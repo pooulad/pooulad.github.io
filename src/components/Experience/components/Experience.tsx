be85a860bd9f089e83f41c39c4c0d5fc25f007a7
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-12 18:43:09 +0330

    setup experience component

---
+++ src/components/Experience/components/Experience.tsx
@@ -0,0 +1,13 @@
+import React, { memo } from "react";
+
+function Experience() {
+  return (
+    <div>
+      <div>Experience</div>
+      <div>Experience</div>
+      <div>Experience</div>
+    </div>
+  );
+}
+
+export default memo(Experience);
