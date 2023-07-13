ba93748d029a1a595a98634f7072b7a7803e96d2
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 02:49:54 +0330

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -119,7 +119,11 @@ function Header() {
         </nav>
       </div>
       {isOpen ? (
-        <div className="min-w-[70vw] z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32 flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32 flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        >
           <nav className="flex flex-col justify-center items-center">
             <CustomLinkMobile
               toggle={handleOpen}
@@ -195,7 +199,7 @@ function Header() {
               <AmirankalaIcon />
             </motion.a>
           </nav>
-        </div>
+        </motion.div>
       ) : null}
     </header>
   );
