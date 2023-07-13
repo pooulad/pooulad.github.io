f8ccd27f7ace634481467dae077ade7f648df82f
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 02:48:21 +0330

    Update CustomLinkMobile.tsx

--- src/components/Common/CustomLinkMobile/CustomLinkMobile.tsx
+++ src/components/Common/CustomLinkMobile/CustomLinkMobile.tsx
@@ -20,12 +20,12 @@ function CustomLinkMobile({
     router.push(link);
   };
   return (
-    <button onClick={handleClick} className={`${classname} relative group text-light dark:text-dark`}>
+    <button onClick={handleClick} className={`${classname} relative group text-light dark:text-dark my-2`}>
       {title}
       <span
-        className={`h-[1px] inline-block bg-dark absolute right-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+        className={`h-[1px] inline-block bg-light absolute right-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
           router.asPath === link ? "w-full" : "w-0"
-        } dark:bg-light`}
+        } dark:bg-dark`}
       >
         &nbsp;
       </span>
