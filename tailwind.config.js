956a3fc7692eec65a2357cc5a4a54dd151425c0c
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 18:24:51 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         circularLight:
           "repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px,#f5f5f5 100px);",
         circularDark:
-          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 5px,#1b1b1b 100px);",
+          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 8px,#1b1b1b 100px);",
       },
     },
   },
