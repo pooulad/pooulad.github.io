83c83efc992b7cbdb62aa4bec2d9362d2da2e583
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 17:47:27 +0330

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,18 @@
 import Layout from "@/Layout";
 import { dictionary } from "@/dictionary";
-import Image from "next/image";
 import Link from "next/link";
 import React, { memo } from "react";
 
 function Footer() {
   return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light">
       <Layout className="py-8 flex items-center justify-between">
         <span>
           {new Date().getFullYear()} &copy; {dictionary.footer.copyRight}
         </span>
         <div className="flex items-center">
           <span>{dictionary.footer.madeWith}</span>
-          <span className="text-primary text-2xl px-1">&#9825;</span>
+          <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span>
           <span>
             {dictionary.footer.by} &nbsp;
             <Link
