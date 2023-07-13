ffe5ebb625f1898a3eb5705b6184c19468ffd10b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 02:51:24 +0330

    Update index.tsx

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -8,7 +8,7 @@ type LayoutProps = {
 function Layout({ children, className }: LayoutProps) {
   return (
     <div
-      className={`w-full h-full inline-block z-0 bg-light p-32 dark:bg-dark ${className}`}
+      className={`w-full h-full inline-block z-0 bg-light p-32 dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
     >
       {children}
     </div>
