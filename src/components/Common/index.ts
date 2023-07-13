481f5d006b6b364a6d5af7f3a0d86b1a3058b8ff
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 02:25:46 +0330

    Update index.ts

--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -1,1 +1,2 @@
-export {default as CustomLink} from "./CustomLink/CustomLink"
+export { default as CustomLink } from "./CustomLink/CustomLink";
+export { default as CustomLinkMobile } from "./CustomLinkMobile/CustomLinkMobile";
