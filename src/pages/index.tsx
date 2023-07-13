2e0c72c6bd8e570a6d7ce507453d7a08403ac4e2
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 03:11:01 +0330

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
         <Layout className="pt-0 md:pt-16 sm:pt-8">
           <div className="flex items-center justify-between w-full lg:flex-col">
-            <div className="w-1/2 px-2">
+            <div className="w-1/2 px-2 md:w-[75%] lg:w-[50%]">
               <Image
                 src={ProfilePic}
                 alt={dictionary.owner}
@@ -38,7 +38,7 @@ export default function Home() {
               <p className="my-4 text-base font-medium">
                 {dictionary.home.pTag}
               </p>
-              <div className="flex items-center self-start mt-2">
+              <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
                   download={true}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:text-dark  hover:bg-light border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light"
@@ -60,7 +60,7 @@ export default function Home() {
           </div>
         </Layout>
         <Hireme />
-        <div className="absolute w-24 inline-block bottom-8 right-8">
+        <div className="absolute w-24 inline-block bottom-8 right-8 md:hidden">
           <Image
             className="w-full h-auto"
             alt={dictionary.ownerEn}
