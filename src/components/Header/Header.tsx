b3f4e10aded35d406614f35919c2f3f21a0b6ab5
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 01:53:32 +0330

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,84 +14,110 @@ import { motion } from "framer-motion";
 import useThemeSwithcher from "@/hooks/useThemeSwithcher";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
   const [mode, setMode] = useThemeSwithcher();
   return (
     <header className="w-full px-32 py-8 font-medium flex justify-between items-center dark:text-light">
-      <nav className="flex items-center justify-center flex-wrap">
-        <CustomLink
-          title={dictionary.header.links.home}
-          link={"/"}
-          classname="mr-4"
-        />
-        <CustomLink
-          title={dictionary.header.links.about}
-          link={"/about"}
-          classname="mx-4"
-        />
-        <CustomLink
-          title={dictionary.header.links.projects}
-          link={"/projects"}
-          classname="ml-4"
-        />
-        <button
-          onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className={"rounded-full flex items-center p-1 justify-center"}
-        >
-          {mode === "dark" ? (
-            <SunToMoonIcon className="fill-dark" />
-          ) : (
-            <MoonToSubIcon className="fill-dark" />
-          )}
-          &nbsp;
-        </button>
-      </nav>
-      <nav className="flex items-center justify-center flex-wrap">
-        <motion.a
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-          href={dictionary.ownerSocials.githubAddress}
-          target={"_blank"}
-        >
-          <GithubIcon />
-        </motion.a>
-        <motion.a
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-          href={dictionary.ownerSocials.telegramAddress}
-          target={"_blank"}
-        >
-          <TelegramIcon />
-        </motion.a>
-        <motion.a
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-          href={dictionary.ownerSocials.instagramAddress}
-          target={"_blank"}
-        >
-          <InstagramIcon />
-        </motion.a>
-        <motion.a
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-          href={`mailto:${dictionary.ownerSocials.emailAddress}`}
-          target={"_blank"}
-        >
-          <EmailIcon />
-        </motion.a>
-        <motion.a
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-          href={dictionary.ownerSocials.websiteAddress}
-          target={"_blank"}
-        >
-          <AmirankalaIcon />
-        </motion.a>
-      </nav>
+      <button
+        onClick={handleOpen}
+        className="flex-col justify-center items-center hidden lg:flex"
+      >
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          }`}
+        ></span>
+      </button>
+      <div className="w-full flex justify-between items-center lg:hidden">
+        <nav className="flex items-center justify-center flex-wrap">
+          <CustomLink
+            title={dictionary.header.links.home}
+            link={"/"}
+            classname="mr-4"
+          />
+          <CustomLink
+            title={dictionary.header.links.about}
+            link={"/about"}
+            classname="mx-4"
+          />
+          <CustomLink
+            title={dictionary.header.links.projects}
+            link={"/projects"}
+            classname="ml-4"
+          />
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={"rounded-full flex items-center p-1 justify-center"}
+          >
+            {mode === "dark" ? (
+              <SunToMoonIcon className="fill-dark" />
+            ) : (
+              <MoonToSubIcon className="fill-dark" />
+            )}
+            &nbsp;
+          </button>
+        </nav>
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+            href={dictionary.ownerSocials.githubAddress}
+            target={"_blank"}
+          >
+            <GithubIcon />
+          </motion.a>
+          <motion.a
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+            href={dictionary.ownerSocials.telegramAddress}
+            target={"_blank"}
+          >
+            <TelegramIcon />
+          </motion.a>
+          <motion.a
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+            href={dictionary.ownerSocials.instagramAddress}
+            target={"_blank"}
+          >
+            <InstagramIcon />
+          </motion.a>
+          <motion.a
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+            href={`mailto:${dictionary.ownerSocials.emailAddress}`}
+            target={"_blank"}
+          >
+            <EmailIcon />
+          </motion.a>
+          <motion.a
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+            href={dictionary.ownerSocials.websiteAddress}
+            target={"_blank"}
+          >
+            <AmirankalaIcon />
+          </motion.a>
+        </nav>
+      </div>
     </header>
   );
 }
