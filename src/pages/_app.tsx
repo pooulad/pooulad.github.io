6007dde35bb727eb65d8c2fe933669a53d59ed0b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 17:31:25 +0330

    add dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-light w-full min-h-screen">
+      <main className="bg-light w-full min-h-screen dark:bg-dark">
         <Header />
         <Component {...pageProps} />
         <Footer />
