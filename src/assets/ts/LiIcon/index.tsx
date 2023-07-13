a7ff37f342dc7437c6525d9971de26dd7bb8f4d6
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 18:24:43 +0330

    Update index.tsx

--- src/assets/ts/LiIcon/index.tsx
+++ src/assets/ts/LiIcon/index.tsx
@@ -6,22 +6,27 @@ export default function LiIcon({ reference }: any) {
     offset: ["center end", "center center"],
   });
   return (
-    <figure className="stroke-dark absolute right-0">
+    <figure className="stroke-dark absolute right-0 dark:stroke-light">
       <svg className="-rotate-90" width="75" height="75" viewBox="0 0 100 100">
         <circle
           cx="75"
           cy="50"
           r="20"
-          className="stroke-primary stroke-1 fill-none"
+          className="stroke-primary dark:stroke-primaryDark stroke-1 fill-none"
         />
         <motion.circle
           style={{ pathLength: scrollYProgress }}
           cx="75"
           cy="50"
           r="20"
-          className="stroke-[5px] fill-light"
+          className="stroke-[5px] fill-light dark:fill-dark"
         />
-        <circle cx="75" cy="50" r="10" className="stroke-1 fill-primary animate-pulse" />
+        <circle
+          cx="75"
+          cy="50"
+          r="10"
+          className="stroke-1 fill-primary dark:fill-primaryDark animate-pulse"
+        />
       </svg>
     </figure>
   );
