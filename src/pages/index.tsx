29a5745061166fb2fef57eb0267d9fe05785387d
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 14:47:08 +0330

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,14 +21,14 @@ export default function Home() {
       <main className="flex items-center text-dark w-full min-h-screen">
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
-            <div className="w-1/2">
+            <div className="w-1/2 px-2">
               <Image
                 src={ProfilePic}
                 alt={dictionary.owner}
                 className="w-full h-auto"
               />
             </div>
-            <div className="w-1/2 flex flex-col items-center self-center">
+            <div className="w-1/2 px-2 flex flex-col items-center self-center">
               <AnimatedText
                 text={dictionary.home.h1Tag}
                 className="!text-6xl !text-right"
