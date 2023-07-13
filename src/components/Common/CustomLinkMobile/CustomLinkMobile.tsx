73cf18aa320c6322598501ae300a968a014dcf35
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 02:25:43 +0330

    Create CustomLinkMobile.tsx

---
+++ src/components/Common/CustomLinkMobile/CustomLinkMobile.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import React, { memo } from "react";
+
+export type CustomLinkMobileProps = {
+  title: string;
+  classname?: string;
+  link: string;
+  toggle: () => void;
+};
+
+function CustomLinkMobile({
+  title,
+  classname,
+  link,
+  toggle,
+}: CustomLinkMobileProps) {
+  const router = useRouter();
+  const handleClick = () => {
+    toggle();
+    router.push(link);
+  };
+  return (
+    <button onClick={handleClick} className={`${classname} relative group text-light dark:text-dark`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute right-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === link ? "w-full" : "w-0"
+        } dark:bg-light`}
+      >
+        &nbsp;
+      </span>
+    </button>
+  );
+}
+
+export default memo(CustomLinkMobile);
