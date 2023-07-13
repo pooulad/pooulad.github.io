9d6429261f46b2309d3b6a2021e33575e949112c
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 18:00:11 +0330

    Update Hireme.tsx

--- src/components/Hireme/Hireme.tsx
+++ src/components/Hireme/Hireme.tsx
@@ -7,7 +7,7 @@ function Hireme() {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
       <div className="w-48 flex items-center justify-center relative h-auto">
-        <HiremeIcon className="fill-dark animate-spin-slow" />
+        <HiremeIcon className="fill-dark animate-spin-slow dark:bg-light dark:rounded-full" />
         <Link
           className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light"
           href={`mailto:${dictionary.ownerSocials.emailAddress}`}
