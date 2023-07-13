f58450af196fb475901a066ce8ed2d9cdb13ed33
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 18:33:42 +0330

    Update index.tsx

--- src/assets/ts/AmirankalaIcon/index.tsx
+++ src/assets/ts/AmirankalaIcon/index.tsx
@@ -3,6 +3,8 @@ export default function AmirankalaIcon() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 608 608"
+      width="1.5em"
+      height="1.5em"
     >
       <path
         fill="#fbfbfb"
