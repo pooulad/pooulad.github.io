ffaee0aec4148c67bc8a54ae59a1e8d0b49199b6
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-13 14:51:16 +0330

    start dark mode in project

---
+++ src/hooks/useThemeSwithcher.tsx
@@ -0,0 +1,13 @@
+import React, { memo } from "react";
+
+function useThemeSwithcher() {
+  return (
+    <div>
+      <div>useThemeSwithcher</div>
+      <div>useThemeSwithcher</div>
+      <div>useThemeSwithcher</div>
+    </div>
+  );
+}
+
+export default memo(useThemeSwithcher);
