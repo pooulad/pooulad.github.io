65ba16f550e880d181b76ffd3271cba74756294a
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 15:02:43 +0330

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -38,7 +38,7 @@ const FeaturedProject = ({
 }: FeaturedProjectProps) => {
   return (
     <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light dark:bg-dark dark:border-light shadow-2xl p-12 relative rounded-br-2xl lg:flex-col lg:p8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light" />
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light xs:-left-2 xs:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
       <Link
         className="w-1/2 overflow-hidden cursor-pointer rounded-lg lg:w-full"
         href={link}
