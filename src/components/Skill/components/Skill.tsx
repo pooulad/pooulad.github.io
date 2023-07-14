559aeba489cc40a480443a18e1cc44bd7b508899
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-14 13:10:16 +0330

    Update Skill.tsx

--- src/components/Skill/components/Skill.tsx
+++ src/components/Skill/components/Skill.tsx
@@ -11,7 +11,7 @@ const SkillGenerator = ({ name, x, y }: SkillGeneratorProps) => {
   return (
     <motion.div
       whileHover={{ scale: 1.05 }}
-      className="cursor-pointer flex items-center justify-center rounded-full font-semibold absolute bg-dark text-light dark:text-dark dark:bg-light py-3 px-6 shadow-dark"
+      className="cursor-pointer flex items-center justify-center rounded-full font-semibold absolute bg-dark text-light dark:text-dark dark:bg-light py-3 px-6 shadow-dark lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:font-bold xs:text-dark xs:dark:bg-light"
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
       viewport={{ once: true }}
@@ -24,15 +24,19 @@ const SkillGenerator = ({ name, x, y }: SkillGeneratorProps) => {
 function Skill() {
   return (
     <Fragment>
-      <h2 className="font-bold text-8xl mt-64 text-center">
+      <h2 className="font-bold text-8xl mt-64 text-center md:text-6xl md:mb-32">
         {dictionary.about.skill.title}
       </h2>
       <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark">
-        <SkillGenerator
-          name={dictionary.about.skill.fullStack}
-          x="0vw"
-          y="0vw"
-        />
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          className="cursor-pointer flex items-center justify-center rounded-full font-semibold absolute bg-dark text-light dark:text-dark dark:bg-light py-3 px-6 shadow-dark lg:p-6 md:p-4 xs:text-sm xs:p-2"
+          initial={{ x: 0, y: 0 }}
+          whileInView={{ x: "0vw", y: "0vw", transition: { duration: 1.5 } }}
+          viewport={{ once: true }}
+        >
+          {dictionary.about.skill.fullStack}
+        </motion.div>
         <SkillGenerator name={dictionary.about.skill.web} x="-20vw" y="2vw" />
         <SkillGenerator
           name={dictionary.about.skill.html}
